55ae1d9480c325f927511565d4b82e0d464d7144
josalazar <53492638+devjosalazar@users.noreply.github.com>
2023-08-15 21:15:59 -0500

    creacion proyecto con VSC

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -2,6 +2,6 @@ package co.edu.uniquindio.biblioteca;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola a todos");
     }
 }
