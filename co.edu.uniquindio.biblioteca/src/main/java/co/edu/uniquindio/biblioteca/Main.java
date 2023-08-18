39eac2f6d3795a03358b0e0ed6b54973357149eb
josalazar <53492638+devjosalazar@users.noreply.github.com>
2023-08-17 19:58:45 -0500

    clase 17-08-2023

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -1,7 +1,60 @@
 package co.edu.uniquindio.biblioteca;
 
+import co.edu.uniquindio.biblioteca.model.Libro;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola a todos");
+        
+        crearVariables();
+        crearLibros();
+
+
     }
+
+    private static void crearLibros() {
+        //crear los 5 libros
+        Libro libro1 = new Libro();
+        libro1.setNombre("suma");
+        libro1.setEditorial("norma");
+
+        Libro libro2 = new Libro();
+        Libro libro3 = new Libro();
+        Libro libro4 = new Libro();
+        Libro libro5 = new Libro();
+        //Obtener la cantidad de libros donde el título empiece por una vocal y consonante.
+        calcularVocalesYConsonantes(libro1, libro2, libro3,libro4,libro5);
+
+
+
+    }
+
+    public static void calcularVocalesYConsonantes(Libro libro1, Libro libro2, Libro libro3, Libro libro4, Libro libro5){
+        int cantidadTerror = 0;
+        int cantidadConsonantes = 0;
+
+        if(libro1.getGenero().equals("Terror")){
+            cantidadTerror = cantidadTerror + 1;
+        }
+
+
+
+
+    }
+
+
+
+    public static void crearVariables(){
+        int edad = 25;
+        double precio = 2000.5;
+        boolean estado = true;
+        String concatenado = "Mi edad es: "+ edad+ " y gano \n: "+precio+ " y estoy"+estado;
+        System.out.println(concatenado);
+    }
+    
+    
 }
+
+
+
+
--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Libro.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Libro.java
@@ -5,6 +5,8 @@ public class Libro {
     private String genero;
     private String autor;
     private int anioPublicacion;
+    private String editorial;
+    private int numeroPagina;
 
     public String getNombre() {
         return nombre;
@@ -36,5 +38,21 @@ public class Libro {
 
     public void setAnioPublicacion(int anioPublicacion) {
         this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public int getNumeroPagina() {
+        return numeroPagina;
+    }
+
+    public void setNumeroPagina(int numeroPagina) {
+        this.numeroPagina = numeroPagina;
     }
 }
