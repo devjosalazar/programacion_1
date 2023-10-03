c4b8ebe5182dd81c8fce129df4ff1dfc5fc3849b
josalazar <53492638+devjosalazar@users.noreply.github.com>
2023-10-02 23:05:53 -0500

    bancoTest_1: test saldo cuenta

--- co.edu.uniquindio.programacion1/src/test/java/co/edu/uniquindio/programacion1/model/CuentaTest.java
+++ co.edu.uniquindio.programacion1/src/test/java/co/edu/uniquindio/programacion1/model/CuentaTest.java
@@ -23,11 +23,14 @@ class CuentaTest {
         String real = cuenta.getNombrePersona();
         assertEquals(esperado, real);
         assertTrue(real.equals("oscar"));
+        assertNotEquals(esperado, real);
     }
 
     @Test
     void testSaldoCuenta(){
         Cuenta cuenta = new Cuenta("Pedro", 200000.0);
+        assertEquals(200000.0, cuenta.getSaldo());
+//        assertFalse(cuenta.getSaldo() <= 0);
+//        assertTrue(cuenta.getSaldo() >= 0);
     }
-
 }
